e31c549e665b77fd198534b2698a828ab4bd2233
roriGit <rori.madonko@gmail.com>
2025-11-20 10:14:59 +0200

    add web server example

---
+++ 1-webserver.js
@@ -0,0 +1,12 @@
+http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url=='/'){
+        res.end(`Home`)
+    }else if(req.url=='/about'){
+        res.end('About')
+    }else {
+        res.end('Error');
+    }
+})
+server.listen(3000);
