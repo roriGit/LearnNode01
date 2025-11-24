c6d3a403667ee79f3002da09879fe10e85126d22
roriGit <rori.madonko@gmail.com>
2025-11-24 09:37:36 +0200

    Misc updates and minor cleanups

--- basic.js
+++ basic.js
@@ -7,6 +7,14 @@
 // process    - info about env where the program is being executed
 const util_func = require('./utils')
 const names = require('./family-info')
+const _lodash = require('lodash')
+const fav = require('./fav')
 
 util_func.hello(names.father)
 util_func.goodbye(names.mother)
+console.log(fav.singleFav)
+
+
+items = [1, [2],[3,4],[5,6,7]]
+newItems = _lodash.flatten(items)
+console.log(newItems)
